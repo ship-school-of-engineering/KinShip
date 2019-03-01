ad0fdc10af978da5a03955eae79bbf853408c93e
Evan Schoenberger <evanjschoenberger@gmail.com>
2019-02-28 20:30:26 -0800

    Add handling for environment variable config, empty command methods

--- KinShip.py
+++ KinShip.py
@@ -16,11 +16,18 @@ logging.basicConfig(level=logging.INFO)
 client = Bot(description="KinShip! - For Ship, By Ship, With Love.", command_prefix="!", pm_help=True)
 
 # Set up configuration
-dev_id = 'Error'
 token = 'Error'
-settings = configparser.RawConfigParser()
-settings.read('config.cfg')
-token = settings.get('Settings', 'token')
+is_production = False
+try:
+    is_production = os.environ['IS_PRODUCTION']
+except KeyError:
+    print('Environment Variable "IS_PRODUCTION" wasn\'t found! Using config file instead.')
+if is_production:
+    token = os.environ['TOKEN']
+else:
+    settings = configparser.RawConfigParser()
+    settings.read('config.cfg')
+    token = settings.get('Settings', 'token')
 
 # This is what happens everytime the bot launches. In this case, it prints information
 # like server count, user count the bot is connected to, and the bot id in the console.
@@ -38,9 +45,28 @@ async def on_ready():
     print('--------')
     await client.change_presence(game=discord.Game(name="Being neat."))
 
+# Send a welcome message with rules/instructions to new members when they join the server.
+# This is not a command. Should be called when the bot sees the event of a member joining.
+async def welcome():
+    pass
+
+###################
+## USER COMMANDS ##
+###################
+
 # Basic example of a command to make sure the bot is working.
 @client.command(hidden=True)
 async def ping():
     await client.say(":thumbsup: Yep, I'm awake!")
+
+# Assign course role
+@client.command()
+async def enroll(course=None):
+    pass
+
+# Assign crew roll
+@client.command
+async def crew():
+    pass
 
 client.run(token)
